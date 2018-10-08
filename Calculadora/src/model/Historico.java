8beeb209698713672c7d7485c3df0e5189ba3406
04646777266 <04646777266@JIPA-LABIN03-35.ifro.local>
2018-10-08 13:20:59 -0400

    a

---
+++ Calculadora/src/model/Historico.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author 04646777266
+ */
+public class Historico {
+    private Integer id;
+    private double primeiroValor;
+    private double segundoValor;
+    
+}
