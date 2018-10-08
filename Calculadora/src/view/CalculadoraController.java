9113d4100ca9f48f570f5ca3af0e6dfb8e300f1a
04026720209 <04026720209@JIPA-LABIN03-33.ifro.local>
2018-10-08 13:43:58 -0400

    opa

---
+++ Calculadora/src/view/CalculadoraController.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author 04026720209
+ */
+public class CalculadoraController implements Initializable {
+
+    @FXML
+    private TextField txtNum1;
+    @FXML
+    private TextField txtNum2;
+    @FXML
+    private TextField txtResultado;
+    @FXML
+    private TextField txtOperador;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void calcular(ActionEvent event) {
+        
+    }
+
+    
+}
